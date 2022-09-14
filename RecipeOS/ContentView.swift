d3a1c8db2eecf983166d22f6f2c261217616d257
Tornelius <tkbjr28@icloud.com>
2022-09-14 09:27:57 -0400

    Initial Commit

---
+++ RecipeOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeOS
+//
+//  Created by Tornelius Broadwater, Jr on 9/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeOS/RecipeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeOSApp.swift
+//  RecipeOS
+//
+//  Created by Tornelius Broadwater, Jr on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
